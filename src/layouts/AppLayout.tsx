e163beb6b85e1017e97a96cf5c2f781e68130e00
afgomezv <afgomezvonline@gmail.com>
2025-03-20 09:10:06 -0500

    auth routes

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -1,10 +1,16 @@
-import { Link, Outlet } from "react-router-dom";
+import { Link, Navigate, Outlet } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Logo from "@/components/Logo";
 import NavMenu from "@/components/NavMenu";
+import { useAuth } from "@/hooks/useAuth";
 
 export default function AppLayout() {
+  const { data, isError, isLoading } = useAuth();
+
+  if (isLoading) return <div>Loading...</div>;
+  if (isError) return <Navigate to="/auth/login" />;
+
   return (
     <>
       <header className="bg-white py-5 shadow">
